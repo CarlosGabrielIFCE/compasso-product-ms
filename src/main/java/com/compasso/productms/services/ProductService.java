05f1e851ff04bfb723333ba8ad9e9240f7a3a63c
Carlos Gabriel <carloscms2009@gmail.com>
2021-06-12 17:55:24 -0300

    Integração da primary key string

--- src/main/java/com/compasso/productms/services/ProductService.java
+++ src/main/java/com/compasso/productms/services/ProductService.java
@@ -16,11 +16,11 @@ public interface ProductService {
 	
 	public abstract ResponseEntity<?> save(Product product);
 	
-	public abstract ResponseEntity<?> update(long id, Product product);
+	public abstract ResponseEntity<?> update(String id, Product product);
 	
-	public abstract ResponseEntity<?> findById(long id);
+	public abstract ResponseEntity<?> findById(String id);
 	
-	public abstract ResponseEntity<?> delete(long id);
+	public abstract ResponseEntity<?> delete(String id);
 	
 	public abstract List<Product> findByDateAndNameOrDescription(String min_value, String max_value, String q);
 	
